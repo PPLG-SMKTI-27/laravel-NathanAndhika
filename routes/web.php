39e99f74131988ee6e5aac44e9c192e6a760c6b4
nathan andik <24_nathandika@student.smkti.net>
2026-03-23 13:50:47 +0800

    baikin error part1

--- routes/web.php
+++ routes/web.php
@@ -35,6 +35,5 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-
-Route::get('/project', [projectController::class, 'index']);
+Route::get('/project', [ProjectController::class, 'index']);
 require __DIR__ . '/auth.php';
