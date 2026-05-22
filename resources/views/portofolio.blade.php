819e3ea414cbb2ae694f6ce92e3e4966744bfbe7
nathan andik <24_nathandika@student.smkti.net>
2026-05-22 10:56:59 +0800

    update foto

--- resources/views/portofolio.blade.php
+++ resources/views/portofolio.blade.php
@@ -582,7 +582,7 @@
                             <div
                                 class="photo-frame relative p-2 bg-slate-900/80 backdrop-blur-sm rounded-xl border border-white/10">
                                 <img src="/foto.jpeg" alt="Nathan Andika"
-                                    class="w-64 h-64 md:w-80 md:h-80 object-cover rounded-lg grayscale-[20%] group-hover:grayscale-0 group-hover:scale-[1.02] transition-all duration-500">
+                                    class="w-64 md:w-80 aspect-[6/19] object-cover rounded-lg grayscale-[20%] group-hover:grayscale-0 group-hover:scale-[1.02] transition-all duration-500">
 
                                 <!-- Decorative Elements -->
                                 <div
